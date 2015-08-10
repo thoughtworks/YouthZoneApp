6e61a3443421a91e233efdacf08b2aae3af1a7cb
Gareth Williams <gareth.williams987@gmail.com>
2015-08-10 10:44:13 +0100

    @gazeddyw @cristianivascu: Add shell scripts for initialisation of avd and running test project.

---
+++ android_create_avd.sh
@@ -0,0 +1,8 @@
+echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+
+./android-wait-for-emulator.sh # see https://github.com/gildegoma/chef-android-sdk/blob/master/files/default/android-wait-for-emulator
+adb shell input keyevent 82 &
+
+<upload your apk>
+<run your tests>
