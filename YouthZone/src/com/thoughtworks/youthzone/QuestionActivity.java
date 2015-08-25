c0d7ac8cf6564cb7f8740b5cd3704a6e1d32c7c8
Cristian Ivascu <cristi.ivascu@yahoo.com>
2015-08-25 17:56:32 +0100

    @cristianivascu @gazeddyw: show warning if there are no questions for theme.

--- YouthZone/src/com/thoughtworks/youthzone/QuestionActivity.java
+++ YouthZone/src/com/thoughtworks/youthzone/QuestionActivity.java
@@ -5,11 +5,17 @@ import java.util.List;
 import java.util.Map;
 
 import com.thoughtworks.youthzone.helper.DatastoreFacade;
+import com.thoughtworks.youthzone.helper.Evaluation;
 import com.thoughtworks.youthzone.helper.Outcome;
+import com.thoughtworks.youthzone.helper.ProjectMember;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,9 +31,9 @@ public class QuestionActivity extends Activity {
 	private Map<String, String> questionsToOutcomes;
 	private List<String> questions;
 	private TextView questionTextview;
-	
+
 	private int questionIndex = -1;
-	
+
 	private String currentQuestion;
 
 	private Map<String, Object> outcomeToRating;
@@ -54,16 +60,16 @@ public class QuestionActivity extends Activity {
 		questionsToOutcomes = ((YouthZoneApp) getApplication()).getQuestionsToOutcomes();
 		questions = new ArrayList<String>();
 
-		outcomeToRating =  ((YouthZoneApp) getApplication()).getSelectedInProgressEvaluation().getOutcomesToRatings();
-		
+		outcomeToRating = ((YouthZoneApp) getApplication()).getSelectedInProgressEvaluation().getOutcomesToRatings();
+
 		for (String question : questionsToOutcomes.keySet()) {
 			if (outcomesForTheme.contains(questionsToOutcomes.get(question))) {
 				questions.add(question);
 			}
 		}
-		
+
 		setupNextQuestion();
-		
+
 		ratingBar.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
 			public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
 
@@ -72,26 +78,30 @@ public class QuestionActivity extends Activity {
 			}
 		});
 	}
-	
+
 	private void setupNextQuestion() {
-		
+
 		questionIndex++;
-		
+
 		if (questionIndex < questions.size()) {
 			currentQuestion = questions.get(questionIndex);
 			questionTextview.setText(currentQuestion);
 			String currentOutcome = questionsToOutcomes.get(currentQuestion);
 			ratingBar.setRating((Float) outcomeToRating.get(currentOutcome));
 		} else {
-			Intent intent = new Intent(this, PickOutcomeActivity.class);
-			startActivity(intent);
+			if (questions.isEmpty()) {
+				showWarning();
+			} else {
+				Intent intent = new Intent(this, PickOutcomeActivity.class);
+				startActivity(intent);
+			}
 		}
 	}
-	
+
 	private void setupPreviousQuestion() {
-	
+
 		questionIndex--;
-		
+
 		if (questionIndex >= 0) {
 			currentQuestion = questions.get(questionIndex);
 			questionTextview.setText(currentQuestion);
@@ -106,9 +116,22 @@ public class QuestionActivity extends Activity {
 	public void onNextQuestionClick(View view) {
 		setupNextQuestion();
 	}
-	
+
 	public void onPreviousQuestionClick(View view) {
 		setupPreviousQuestion();
+	}
+
+	private void showWarning() {
+		new AlertDialog.Builder(this).setTitle("Oops ...")
+				.setMessage("There are no questions for this theme.")
+				.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						 dialog.cancel();
+						 Intent intent = new Intent(QuestionActivity.this, PickOutcomeActivity.class);
+						 startActivity(intent);
+						 finish();
+					}
+				}).setIcon(android.R.drawable.ic_dialog_alert).show();
 	}
 
 	@Override
